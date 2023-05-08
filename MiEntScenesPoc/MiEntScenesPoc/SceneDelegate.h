d8610c7754bfc611677a97998ba3542602849dbe
mediaios <iosmediadev@gmail.com>
2023-05-08 11:13:26 +0800

    init prj

---
+++ MiEntScenesPoc/MiEntScenesPoc/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  MiEntScenesPoc
+//
+//  Created by Qi on 2023/5/8.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ MiEntScenesPoc/MiEntScenesPoc/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  MiEntScenesPoc
+//
+//  Created by Qi on 2023/5/8.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
